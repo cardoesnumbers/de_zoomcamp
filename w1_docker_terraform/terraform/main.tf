2b48b22512b5b7256dcdb56e8b48f5f91156b8d7
cardoesnumbers <carlosurtecho@gmail.com>
2026-02-09 22:32:28 +0100

    after this needs to align location to EU or europe-west1

--- w1_docker_terraform/terraform/main.tf
+++ w1_docker_terraform/terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "google" {
   project     = "terraform-demo-485312"
-  region      = "europe-west1"
+  region      = "EU"
 
 
 }
@@ -29,7 +29,16 @@ resource "google_storage_bucket" "demo-bucket" {
   }
 }
 
+resource "google_bigquery_dataset" "zoomcamp" {
+  dataset_id = "zoomcamp"
+  location   = "EU"
+}
+
 output "gcs_bucket_name" {
   value = google_storage_bucket.demo-bucket.name
+}
+
+output "bq_dataset_id" {
+  value = google_bigquery_dataset.zoomcamp.dataset_id
 }
 #name          = "terraform-demo-485312-terra-bucket"
